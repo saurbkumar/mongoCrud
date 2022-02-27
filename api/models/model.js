35df0adb5a5672d48476d50551b2ff1cda899b73
Saurabh Kumar <saurabhkumar.spsu@gmail.com>
2022-02-27 11:16:30 -0800

    code reorg (#3)

--- api/models/model.js
+++ api/models/model.js
@@ -1,7 +1,5 @@
-const mongoHelper = require('../helpers/mongoHelper');
-
 const memoryCacheModel = require('../models/memoryCacheModel');
-const mongoModel = require('../models/mongoDbModel');
+const dbModel = require('../models/mongoDbModel');
 
 module.exports = {
   getUser: getUser,
@@ -10,41 +8,41 @@ module.exports = {
   deleteUser: deleteUser,
   getUsers: getUsers,
   deleteUsers: deleteUsers,
-  start: mongoHelper.connect,
-  close: mongoHelper.close
+  start: dbModel.start,
+  close: dbModel.close
 };
 
 async function getUser(id) {
   let result = memoryCacheModel.getObject(id);
   if (!result) {
-    result = await mongoModel.getUser(id);
-    if (result) memoryCacheModel.createObject(id, result.toJSON());
+    result = await dbModel.getUser(id);
+    if (result) memoryCacheModel.createObject(id, dbModel.copy(result));
   }
   return result;
 }
 
 async function createUser(user) {
-  const result = await mongoModel.createUser(user);
-  memoryCacheModel.createObject(result.get('id'), result.toJSON());
+  const result = await dbModel.createUser(user);
+  memoryCacheModel.createObject(result.get('id'), dbModel.copy(result));
   return result;
 }
 
 async function updateUser(id, user) {
-  const result = await mongoModel.updateUser(id, user);
-  if (result) memoryCacheModel.updateObject(id, result.toJSON());
+  const result = await dbModel.updateUser(id, user);
+  if (result) memoryCacheModel.updateObject(id, dbModel.copy(result));
   return result;
 }
 
 async function deleteUser(id) {
   memoryCacheModel.deleteObject(id);
-  return mongoModel.deleteUser(id);
+  return dbModel.deleteUser(id);
 }
 async function getUsers(top, skip) {
-  let result = mongoModel.getUsers(top, skip);
+  let result = dbModel.getUsers(top, skip);
   return result;
 }
 
 async function deleteUsers() {
   memoryCacheModel.clear();
-  return mongoModel.deleteUsers();
+  return dbModel.deleteUsers();
 }
